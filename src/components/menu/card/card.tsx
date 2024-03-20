5f6db68966f7b294c9a8d9172378b939d696f9d1
Giovanni Crudele <crudelegio@gmail.com>
2024-03-20 15:57:48 +0100

    fix on card size

--- src/components/menu/card/card.tsx
+++ src/components/menu/card/card.tsx
@@ -7,20 +7,22 @@ import classes from "./card.module.css";
 //the card component will have all the meals properties
 const Card: React.FC<Meals> = ({ image, name, price, description }) => {
   return <div className={classes["meal-item"]}>
-    <article>
+    <div className={classes["article"]}>
         <img src={`${url}${image}`} alt={name}/>
         <h3>{name}</h3>
-        <div>
-            <p className={classes["meal-item-price"]}>€ {price}</p>
+            <div>
+                <p className={classes["meal-item-price"]}>€ {price}</p>
             </div>
-        <p className={classes["meal-item-description"]}>{description}</p>
-        <div>
+      
+         <p className={classes["meal-item-description"]}>{description}</p>
+     
+        <div className={classes["meal-item-actions"]}>
             <Button children="Add to cart" />
 
             {/* fare condizionale per mostrare bottone rimozione elemento */}
             <Button children="X" />
         </div>
-    </article>
+    </div>
   </div>;
 };
 export default Card;
