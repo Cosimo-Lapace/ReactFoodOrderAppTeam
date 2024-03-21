143b160d394c692c4205d79f02023ab3b43c9798
Giovanni Crudele <crudelegio@gmail.com>
2024-03-21 14:12:14 +0100

    implemented routing system

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    // eventuale mappatura delle rotte a livello applicativo con /*
+    path: "/",
+    element: <App />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+       <RouterProvider router={router} />
   </React.StrictMode>,
 )
