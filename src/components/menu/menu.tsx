79ebb4e6bc0084b13d95111a09432e887639a32f
Giovanni Crudele <crudelegio@gmail.com>
2024-03-21 15:18:31 +0100

    filter search!

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { getMenu } from "../../http/https.ts";
 import  Card from "../../components/menu/card/card.tsx";
 import { Meals } from "../../model/Meals.ts";
@@ -5,19 +6,37 @@ import classes from "./menu.module.css";
 import useApi from "../../hooks/useAPI.tsx";
 import Spinner from "../../utilities/spinner/spinner.tsx";
 import Button from "../../utilities/button/button.tsx";
+import Input from "../../utilities/input/input";
 
 const Menu: React.FC = ()=>{
     //we destructure the useApi hook and pass the getMeals function
     const {data:meals, error, loading} = useApi(getMenu, "meals", [], "An error has occurred");
-    
+
+    //State searchbar:
+    const [searchQuery, setSearchQuery] = useState<string>("");
+
+    //filter meals:
+    const filteredMeals = meals.filter((meal: Meals) => meal.name.toLowerCase().includes(searchQuery.toLowerCase()));
+   
+    //handleSearch function:
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchQuery(event.target.value);
+    };
+
+    //display meals at the occurrance of the search query:
+    const displayMeals = searchQuery.trim() ? filteredMeals : meals;
+
     return(
     <>
+    <div className={classes["search-container"]}>
+        <Input name={"search"} type={"text"} placeholder={"search..."} onChange={handleSearch} />
+    </div>
     {/* spinner container if the data is loading */}
     {loading ? (<Spinner typeContainer="xl" />) : null}
     {/* error container if the data is not loading and there is an error with retry button */}
     {error ? (<div className={classes["error-container"]}><h5>{error}</h5><Button children="Retry" onClick={() => window.location.reload()} /></div>) : null}
         {!error && !loading ? (<div id={classes.meals}>
-            {meals.map((meal: Meals) => (
+            {displayMeals.map((meal: Meals) => (
                 // we pass the list of the meals through a mapped array
                 <div key={meal.id}>
                     <Card id={meal.id} image={meal.image} name={meal.name} price={meal.price} description={meal.description} />
