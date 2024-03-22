57450a379e82335bb6fee199ed87cad5418eb4ab
ddomenici98 <domenicidaniele00@gmail.com>
2024-03-22 16:56:40 +0100

    conclusion css

--- src/components/conclusion/conclusion.tsx
+++ src/components/conclusion/conclusion.tsx
@@ -1,11 +1,11 @@
 import { useNavigate } from "react-router";
-import style from "./conclusion.module.css";
+import  "./conclusion.css";
 import Button from "../../utilities/button/button";
 
 function Conclusion() {
   const navigate = useNavigate();
   return (
-    <div className={style.conclusion}>
+    <div className="conclusion">
       <h1>Thank you for your order!</h1>
       <Button onClick={() => navigate("/")}>Close</Button>
     </div>
