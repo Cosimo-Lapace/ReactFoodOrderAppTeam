945439d9635ad8a23f7bb24a21e5691d42d464fb
Cosimo Lapace <120408625+Cosimo-Lapace@users.noreply.github.com>
2024-03-25 14:57:26 +0100

    cy

--- cypress/integration/test/test.ts
+++ cypress/integration/test/test.ts
@@ -1,16 +1,13 @@
 /// <reference types="cypress" />
 
-describe("test",()=>{
-
-    it("test",()=>{
-
-          cy.visit(Cypress.env("baseUrl"));
-          cy.wait(2000);
-          cy.get('h1').should('be.visible');
-          cy.get('h1').should('have.text','ReactFood');
-          cy.get('.article').should('have.length',20);
-          cy.get('.control').type('margherita');
-          cy.get('.article').first().contains('Margherita').contains('button', 'Add to Cart').click();
-    })
-
-})
+describe("test", () => {
+  it("test", () => {
+    cy.visit(Cypress.env("baseUrl"));
+    cy.wait(2000);
+    cy.get("h1").should("be.visible");
+    cy.get("h1").should("have.text", "ReactFood");
+    cy.get(".article").should("have.length", 20);
+    cy.get(".control").type("margherita");
+    cy.get(".article").first().find("h3");
+  });
+});
