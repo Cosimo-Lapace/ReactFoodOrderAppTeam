b24a7229a746e892dae998e6ae6510766395f56c
Giovanni Crudele <crudelegio@gmail.com>
2024-03-25 15:18:00 +0100

    tests of test

--- cypress/integration/test/test.ts
+++ cypress/integration/test/test.ts
@@ -8,6 +8,17 @@ describe("test", () => {
     cy.get("h1").should("have.text", "ReactFood");
     cy.get(".article").should("have.length", 20);
     cy.get(".control").type("margherita");
-    cy.get(".article").first().find("h3");
+    cy.get(".article").first().find("h3").then(($element) => {
+        if($element.text() === "Margherita") {
+        cy.get('.article').first().find('button').click();
+        }
+    });
+    cy.get('#main-header > :nth-child(2) > :nth-child(2)').then(($element) => {
+        // if($element.text().includes("1")) {
+        //   cy.wrap($element).click(); 
+        // }
+        cy.log($element.text());
+    })
+    // cy.get('dialog').should('be.visible');
   });
 });
